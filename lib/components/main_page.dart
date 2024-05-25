ef43448034d77929a2704cd9f16d8059f7d565cd
Fathir Akmal Burhanudin <starboyaja@gmail.com>
2024-05-25 13:44:09 +0700

    update: content

--- lib/components/main_page.dart
+++ lib/components/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:absensi_mobile/data/data.dart';
+import 'package:absensi_mobile/detail_presence.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -12,11 +13,10 @@ class TapIn extends StatefulWidget {
 class _TapInState extends State<TapIn> {
   @override
   Widget build(BuildContext context) {
-      double widthScreen = MediaQuery.of(context).size.width;
+    double widthScreen = MediaQuery.of(context).size.width;
     double heightScreen = MediaQuery.of(context).size.height;
     double font12 = widthScreen * 0.038;
     double font14 = widthScreen * 0.042;
-
 
     return Container(
       margin: EdgeInsets.symmetric(horizontal: widthScreen * 0.06),
@@ -29,90 +29,115 @@ class _TapInState extends State<TapIn> {
           const SizedBox(height: 15),
           Column(
             children: [
-              Container(
-                margin: const EdgeInsets.only(bottom: 20),
-                width: widthScreen,
-                height: widthScreen * 0.28,
-                padding: const EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                    color: whiteBg, borderRadius: BorderRadius.circular(10)),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Senin 12 januari 2024',
-                            style: TextStyle(fontSize: 12, color: Colors.blue),
-                          ),
-                          Row(children: [
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const DetailPresence(),
+                      ));
+                },
+                child: Container(
+                  margin: const EdgeInsets.only(bottom: 20),
+                  width: widthScreen,
+                  height: widthScreen * 0.28,
+                  padding: const EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                      color: whiteBg, borderRadius: BorderRadius.circular(10)),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
                             Text(
-                              'Berhasil Melakukan ',
-                              style: GoogleFonts.poppins(
-                                fontWeight: textMedium,
-                                  fontSize: 14, color: blackText),
+                              'Senin 12 januari 2024',
+                              style:
+                                  TextStyle(fontSize: 12, color: Colors.blue),
                             ),
+                            Row(children: [
+                              Text(
+                                'Berhasil Melakukan ',
+                                style: GoogleFonts.poppins(
+                                    fontWeight: textMedium,
+                                    fontSize: 14,
+                                    color: blackText),
+                              ),
+                              Text(
+                                ' TAP IN',
+                                style: GoogleFonts.poppins(
+                                    fontWeight: textBold,
+                                    fontSize: 14,
+                                    color: Colors.blue),
+                              ),
+                            ]),
+                            SizedBox(height: 15),
                             Text(
-                              ' TAP IN',
-                              style: GoogleFonts.poppins(
-                                fontWeight: textBold,
-                                  fontSize: 14, color: Colors.blue),
+                              'Time In: 06:40 | Senin, 12 Januari 2024',
+                              style: TextStyle(fontSize: 12),
                             ),
-                          ]),
-                          SizedBox(height: 15),
-                          Text(
-                            'Time In: 06:40 | Senin, 12 Januari 2024',
-                            style: TextStyle(fontSize: 12),
-                          ),
-                        ],
-                      ),
-                      Image.asset('images/GreenCheck.png')
-                    ]),
+                          ],
+                        ),
+                        Image.asset('images/GreenCheck.png')
+                      ]),
+                ),
               ),
-              Container(
-                margin: const EdgeInsets.only(bottom: 20),
-                width: widthScreen,
-                height: widthScreen * 0.28,
-                padding: const EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                    color: whiteBg, borderRadius: BorderRadius.circular(10)),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Senin 12 januari 2024',
-                            style: TextStyle(fontSize: 12, color: Colors.blue),
-                          ),
-                          Row(children: [
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DetailPresence(),
+                      ));
+                },
+                child: Container(
+                  margin: const EdgeInsets.only(bottom: 20),
+                  width: widthScreen,
+                  height: widthScreen * 0.28,
+                  padding: const EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                      color: whiteBg, borderRadius: BorderRadius.circular(10)),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
                             Text(
-                              'Berhasil Melakukan ',
-                              style: GoogleFonts.poppins(
-                                fontWeight: textMedium,
-                                  fontSize: 14, color: blackText),
+                              'Senin 12 januari 2024',
+                              style:
+                                  TextStyle(fontSize: 12, color: Colors.blue),
                             ),
+                            Row(children: [
+                              Text(
+                                'Berhasil Melakukan ',
+                                style: GoogleFonts.poppins(
+                                    fontWeight: textMedium,
+                                    fontSize: 14,
+                                    color: blackText),
+                              ),
+                              Text(
+                                ' TAP IN',
+                                style: GoogleFonts.poppins(
+                                    fontWeight: textBold,
+                                    fontSize: 14,
+                                    color: Colors.blue),
+                              ),
+                            ]),
+                            SizedBox(height: 15),
                             Text(
-                              ' TAP IN',
-                              style: GoogleFonts.poppins(
-                                fontWeight: textBold,
-                                  fontSize: 14, color: Colors.blue),
+                              'Time In: 06:40 | Senin, 12 Januari 2024',
+                              style: TextStyle(fontSize: 12),
                             ),
-                          ]),
-                          SizedBox(height: 15),
-                          Text(
-                            'Time In: 06:40 | Senin, 12 Januari 2024',
-                            style: TextStyle(fontSize: 12),
-                          ),
-                        ],
-                      ),
-                      Image.asset('images/GreenCheck.png')
-                    ]),
-              ),  ],
+                          ],
+                        ),
+                        Image.asset('images/GreenCheck.png')
+                      ]),
+                ),
+              ),
+            ],
           )
         ],
       ),
